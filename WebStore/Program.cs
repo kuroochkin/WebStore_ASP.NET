79d059608f560aafccb6dc3499d82a86d5a02598
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2022-11-30 21:47:12 +0300

    Добавил комментарии

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args); // Создаем builder
+var app = builder.Build(); // Сосздаем приложение
+
+var servises = builder.Services; // Добавляем сервисы
+servises.AddControllersWithViews(); // Добавляем MVC
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // Перехват всех ошибок
+}
+
 
 app.MapGet("/", () => app.Configuration["HelloProject"]);
 
-app.Run();
+app.Run(); // Запуск программы
