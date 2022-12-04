3bcd430724d63500585806f0225223b9b77dbc81
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2022-12-05 01:21:01 +0300

    Добавил представление сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> employees = new()
+        {
+            new Employee {Id = 1, LastName = "Келин", FirstName = "Кирилл", Patronymic = "Вячеславович", Age = 19},
+            new Employee {Id = 2, LastName = "Горбатых", FirstName = "Александр", Patronymic = "Андреевич", Age = 18},
+            new Employee {Id = 3, LastName = "Ярочевский", FirstName = "Максим", Patronymic = "Павлович", Age = 18}
+        };
+
         public IActionResult Index()
         {
             //return Content("Добавление главного контроллера");
@@ -13,6 +21,11 @@ namespace WebStore.Controllers
         public string ConfiguredAction(string id)
         {
             return $"Hello World! {id}";
+        }
+
+        public IActionResult Employees()
+        {
+            return View(employees);
         }
     }
 }
