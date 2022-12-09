03d8324cb8ac1c9a53ed8da7b8f352075ad9c3fa
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2022-12-09 04:23:59 +0300

    Добавил перевод

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> employees = new()
+        {
+            new Employee {Id = 1, LastName = "Келин", FirstName = "Кирилл", Patronymic = "Вячеславович", Age = 19},
+            new Employee {Id = 2, LastName = "Горбатых", FirstName = "Александр", Patronymic = "Андреевич", Age = 18},
+            new Employee {Id = 3, LastName = "Ярочевский", FirstName = "Максим", Patronymic = "Павлович", Age = 18}
+        };
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -5,13 +5,6 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> employees = new()
-        {
-            new Employee {Id = 1, LastName = "Келин", FirstName = "Кирилл", Patronymic = "Вячеславович", Age = 19},
-            new Employee {Id = 2, LastName = "Горбатых", FirstName = "Александр", Patronymic = "Андреевич", Age = 18},
-            new Employee {Id = 3, LastName = "Ярочевский", FirstName = "Максим", Patronymic = "Павлович", Age = 18}
-        };
-
         public IActionResult Index()
         {
             //return Content("Добавление главного контроллера");
@@ -21,11 +14,6 @@ namespace WebStore.Controllers
         public string ConfiguredAction(string id)
         {
             return $"Hello World! {id}";
-        }
-
-        public IActionResult Employees()
-        {
-            return View(employees);
         }
     }
 }
