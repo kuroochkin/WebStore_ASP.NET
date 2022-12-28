dcbe77fddcc317e68c9a38a6a7bd99a08ab5e1c6
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2022-12-28 22:45:21 +0300

    Промежуточное ПО

---
+++ WebStore/Infrastructure/Conventions/TestConvention.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Diagnostics;
+
+namespace WebStore.Infrastructure.Conventions
+{
+    public class TestConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            Debug.WriteLine(controller.ControllerName);
+
+        }
+    }
+}
