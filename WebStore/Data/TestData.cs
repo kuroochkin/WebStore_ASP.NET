1c3bb15e1ecaf38d7429f22ac1df68e03d7da092
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2022-12-28 23:03:20 +0300

    Создание TestData. Переселение сотрудников в TestData.

---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,14 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public class TestData
+    {
+        public static List<Employee> employees { get; } = new()
+        {
+            new Employee {Id = 1, LastName = "Келин", FirstName = "Кирилл", Patronymic = "Вячеславович", Age = 19},
+            new Employee {Id = 2, LastName = "Горбатых", FirstName = "Александр", Patronymic = "Андреевич", Age = 18},
+            new Employee {Id = 3, LastName = "Ярочевский", FirstName = "Максим", Patronymic = "Павлович", Age = 18}
+        };
+    }
+}
--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -6,11 +6,6 @@
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public  string Patronymic { get; set; }
-
         public int Age { get; set; }
-
-
-
-
     }
 }
