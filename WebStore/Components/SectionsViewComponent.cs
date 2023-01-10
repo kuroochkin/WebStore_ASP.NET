f4a502e9840738070908760dc76b9a367d9e6a64
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-10 10:16:10 +0300

    Модель-представления секции

---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+        public SectionsViewComponent(IProductData ProductData)
+        {
+            _ProductData = ProductData;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var section = _ProductData.GetSection(); // Получаемм секции
+
+            return View();
+        }
+    }
+}
---
+++ WebStore/ViewModels/SectionViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebStore.ViewModels
+{
+    public class SectionViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+        public SectionViewModel? Parent { get; set; }
+
+        public List<SectionViewModel> ChildSection { get; set; } = new();
+    }
+}
