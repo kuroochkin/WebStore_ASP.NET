d441088ae998dd8ac956cb0606589a9998e7a783
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-10 17:39:50 +0300

    Добавление товара в сервис каталога товаров

---
+++ WebStore.Domain/ProductFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Domain
+{
+    public class ProductFilter
+    {
+        public int SectionId { get; set; } // Секция, в которой находится продукт
+        public int? BrandId { get; set; } // Бренд продукта
+    }
+}
