4a8ffb1fb961117cf29a694e0503be14411c2d5b
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-10 15:27:07 +0300

    Поправки

--- WebStore/ViewModels/SectionViewModel.cs
+++ WebStore/ViewModels/SectionViewModel.cs
@@ -5,9 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int Order { get; set; }
-
         public SectionViewModel? Parent { get; set; }
-
         public List<SectionViewModel> ChildSection { get; set; } = new();
     }
 }
