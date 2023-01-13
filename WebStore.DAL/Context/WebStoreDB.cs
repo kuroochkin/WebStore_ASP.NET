1ec7499a63480a5573da3dbf97eaa9590bf3baf5
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-13 09:34:40 +0300

    Определение контекста БД

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreDB : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Brand> brands { get; set; }
+        public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options)
+        {
+
+        }
+    }
+}
