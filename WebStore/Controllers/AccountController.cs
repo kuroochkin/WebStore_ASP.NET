2aff6ee39cd93b2804499550eaf1e710c6bf2d88
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-14 13:04:48 +0300

    Заготовка контроллера Account

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register() => View(); // Регистрация
+        public IActionResult Login() => View(); // Вход в систему
+        public IActionResult Logout() => RedirectToAction("Index", "Home"); // Выход из системы
+        public IActionResult AccessDehied() => View(); // Отказ в доступе
+    }
+}
