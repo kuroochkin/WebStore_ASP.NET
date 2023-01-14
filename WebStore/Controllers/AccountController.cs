afd8c5e36621867ef5049fd46141e553d754b0a4
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-14 15:00:12 +0300

    Модели-представления для регистрации и входа в систему

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register() => View(); // Регистрация
+        public IActionResult Register() // Регистрация
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register()//EmployeeViewModel model)
+        {
+
+        }
+
+
         public IActionResult Login() => View(); // Вход в систему
         public IActionResult Logout() => RedirectToAction("Index", "Home"); // Выход из системы
         public IActionResult AccessDenied() => View(); // Отказ в доступе
---
+++ WebStore/ViewModels/Identity/RegisterUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace WebStore.ViewModels.Identity
+{
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Подтверждение пароля")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))] // Должно совпадать с Password
+        public string PasswordConfirm { get; set; } // Подтверждение пароля
+    }
+}
