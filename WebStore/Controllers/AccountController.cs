1752484391f581ee1bc20ded581f56e7d9f03bf3
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-14 15:12:48 +0300

    Логика процесса регистрации нового пользователя

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,19 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using WebStore.Domain.Entities.Identity;
 using WebStore.ViewModels;
+using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register() // Регистрация
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
         {
-            return View();
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Register()//EmployeeViewModel model)
-        {
 
+        public IActionResult Register() => View(new RegisterUserViewModel());// Регистрация
+        
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)//EmployeeViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName,
+            };
+            var registration_result = await _UserManager.CreateAsync(user, Model.Password);
+            if (registration_result.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registration_result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(Model);
         }
 
 
