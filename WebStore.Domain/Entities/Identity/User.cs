be673e9a4debdc530802c6c47ce4fedb2248fbc3
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-19 01:05:32 +0300

    User

--- WebStore.Domain/Entities/Identity/User.cs
+++ WebStore.Domain/Entities/Identity/User.cs
@@ -9,6 +9,8 @@ namespace WebStore.Domain.Entities.Identity
 {
     public class User : IdentityUser
     {
+        public const string Administrator = "Admin";
 
+        public const string DefaultAdminPassword = "AdPAss_123";
     }
 }
