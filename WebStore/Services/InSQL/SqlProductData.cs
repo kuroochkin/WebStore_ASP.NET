9a8e0226dabb76a35b6fa3d357b9406c225962c4
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-24 16:59:00 +0300

    Изменен метод GetProducts в SqlProductData

--- WebStore/Services/InSQL/SqlProductData.cs
+++ WebStore/Services/InSQL/SqlProductData.cs
@@ -16,13 +16,20 @@ namespace WebStore.Services.InSQL
 
         public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
         {
-            IQueryable<Product> query = _db.Products;
+            IQueryable<Product> query = _db.Products
+           .Include(p => p.Brand)
+           .Include(p => p.Section);
 
-            if (Filter?.SectionId is { } section_id) // Накладываем на запрос фильтр
-                query = query.Where(p => p.SectionId == section_id);
+            if (Filter?.Ids?.Length > 0)
+                query = query.Where(product => Filter.Ids.Contains(product.Id));
+            else
+            {
+                if (Filter?.SectionId is { } section_id)
+                    query = query.Where(p => p.SectionId == section_id);
 
-            if (Filter?.BrandId is { } brand_id)
-                query = query.Where(p => p.BrandId == brand_id);
+                if (Filter?.BrandId is { } brand_id)
+                    query = query.Where(p => p.BrandId == brand_id);
+            }
 
             return query;
         }
