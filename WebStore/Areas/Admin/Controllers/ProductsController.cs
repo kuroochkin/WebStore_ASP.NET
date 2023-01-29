f426661286d1bd1de53049d6a50780fd92376a1e
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-29 16:54:57 +0300

    Разметка области администратора

---
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
