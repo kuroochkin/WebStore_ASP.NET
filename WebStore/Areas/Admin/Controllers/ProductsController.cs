d92bebf182786db7795f6b4bcdecf9b261ea7757
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-01-29 17:28:22 +0300

    Добавлены товары в каталог админа

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using WebStore.Domain.Entities.Identity;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
@@ -9,9 +10,21 @@ namespace WebStore.Areas.Admin.Controllers
     [Authorize(Roles = Role.Administrators)]
     public class ProductsController : Controller
     {
+        private readonly IProductData _ProductData;
+        private readonly ILogger<ProductsController> _Logger;
+
+        public ProductsController(IProductData ProductData, ILogger<ProductsController> Logger)
+        {
+            this._ProductData = ProductData;
+            this._Logger = Logger;
+        }
+
+        public IProductData ProductData { get; }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _ProductData.GetProducts();
+            return View(products);
         }
     }
 }
