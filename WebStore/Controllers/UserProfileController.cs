389fe8f60b6397dee72113a5e4df08aea3de97e5
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-01 22:03:19 +0300

    Профиль пользователя с отображением в нем списка заказов

--- WebStore/Controllers/UserProfileController.cs
+++ WebStore/Controllers/UserProfileController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -7,6 +9,21 @@ namespace WebStore.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService OrderService)
+        {
+            var orders = await OrderService.GetUserOrdersAsync(User.Identity!.Name!);
+
+            return View(orders.Select(order => new UserOrderViewModel
+            {
+                Id = order.Id,
+                Address = order.Address,
+                Phone = order.Phone,
+                Description = order.Description,
+                TotalPrice = order.TotalPrice,
+                Date = order.Date,
+            }));
         }
     }
 }
