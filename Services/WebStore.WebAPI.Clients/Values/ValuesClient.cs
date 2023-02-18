0678424aab17b40e9fc72ee8d2a33a806dd3d90e
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-19 02:52:20 +0300

    Заготовка клиента тестового WebAPI

---
+++ Services/WebStore.WebAPI.Clients/Values/ValuesClient.cs
@@ -0,0 +1,43 @@
+﻿using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Values
+{
+    public class ValuesClient : BaseClient, IValuesService
+    {
+        public ValuesClient(HttpClient Client) : base(Client, "api/values")
+        {
+
+        }
+
+        public void Add(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int Id, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
