ae293ca70d0ac758b16592d70f914864ce675aba
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-19 02:40:15 +0300

    Тестовое WebAPI

--- Services/WebStore.WebAPI/Controllers/ValuesController.cs
+++ Services/WebStore.WebAPI/Controllers/ValuesController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.WebAPI.Controllers
 {
@@ -7,6 +6,59 @@ namespace WebStore.WebAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly ILogger<ValuesController> _Logger;
 
+        private static readonly Dictionary<int, string> _Values = Enumerable.Range(1, 10)
+            .Select(i => (Id: i, Value: $"Value-{i}"))
+            .ToDictionary(v => v.Id, v => v.Value);
+
+        public ValuesController(ILogger<ValuesController> Logger) => _Logger = Logger;
+
+        [HttpGet]
+        public IActionResult Get() => Ok(_Values.Values);
+
+        [HttpGet("{Id}")]
+        public IActionResult GetById(int id)
+        {
+            if (!_Values.ContainsKey(id))
+                return NotFound();
+
+            return Ok(_Values[id]);
+        }
+
+        [HttpGet("count")]
+        public int Count() => _Values.Count;
+
+        [HttpPost]
+        [HttpPost("add")]
+        public IActionResult Add([FromBody] string Value)
+        {
+            var id = _Values.Count == 0 ? 1 : _Values.Keys.Max() + 1;
+            _Values[id] = Value;
+
+            return CreatedAtAction(nameof(GetById), new { id });
+        }
+
+        [HttpPut("{Id}")]
+        public IActionResult Replace(int Id, [FromBody] string Value)
+        {
+            if (!_Values.ContainsKey(Id))
+                return NotFound();
+
+            _Values[Id] = Value;
+
+            return Ok();
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            if (!_Values.ContainsKey(Id))
+                return NotFound();
+
+            _Values.Remove(Id);
+
+            return Ok();
+        }
     }
 }
