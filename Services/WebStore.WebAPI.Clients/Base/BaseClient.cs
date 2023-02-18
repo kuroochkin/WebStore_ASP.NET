469b611e6af1851e98a1c7a97a3ff65ec9b74e52
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-19 02:49:56 +0300

    Клас базового клиента

---
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.WebAPI.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected HttpClient Http { get; }
+
+        protected string Address { get; }
+
+        public BaseClient(HttpClient Client, string Address)
+        {
+            Http = Client;
+            this.Address = Address;
+        }
+
+
+    }
+}
