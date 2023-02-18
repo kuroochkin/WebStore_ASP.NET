69c5e68af889485ee2fc9d2f417c60a4d24e0af2
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-18 21:01:41 +0300

    Проект-хостинг для нашего WebAPI

---
+++ Services/WebStore.WebAPI/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var services = builder.Services;
+
+services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
