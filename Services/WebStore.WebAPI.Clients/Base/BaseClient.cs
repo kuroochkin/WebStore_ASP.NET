827e6144ca5596775b7f3c4e18d6c00bbd8415f9
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-26 20:54:33 +0300

    Доработка базового клиента WebAPI

--- Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +13,46 @@ namespace WebStore.WebAPI.Clients.Base
 
         protected string Address { get; }
 
-        public BaseClient(HttpClient Client, string Address)
+        protected BaseClient(HttpClient Client, string Address)
         {
             Http = Client;
             this.Address = Address;
         }
 
+        protected T? Get<T>(string url) => GetAsync<T>(url).Result;
 
+        protected async Task<T?> GetAsync<T>(string url)
+        {
+            var response = await Http.GetAsync(url).ConfigureAwait(false); // в responce получаем ответ от сервера
+            return await response
+                .EnsureSuccessStatusCode()
+                .Content
+                .ReadFromJsonAsync<T>() // десериализация
+                .ConfigureAwait(false);
+        }
+
+        protected HttpResponseMessage Post<T>(string url, T value) => PostAsync<T>(url, value).Result;
+
+        protected async Task<HttpResponseMessage?> PostAsync<T>(string url, T value)
+        {
+            var response = await Http.PostAsJsonAsync(url, value).ConfigureAwait(false); // в responce получаем ответ от сервера
+            return response.EnsureSuccessStatusCode(); 
+        }
+
+        protected HttpResponseMessage Put<T>(string url, T value) => PutAsync<T>(url, value).Result;
+
+        protected async Task<HttpResponseMessage?> PutAsync<T>(string url, T value)
+        {
+            var response = await Http.PutAsJsonAsync(url, value).ConfigureAwait(false); // в responce получаем ответ от сервера
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+
+        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            var response = await Http.DeleteAsync(url).ConfigureAwait(false);
+            return response;
+        }
     }
 }
