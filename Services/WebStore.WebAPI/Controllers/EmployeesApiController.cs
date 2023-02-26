366eebe39d2a34710e8c156e85e35bab8f74380a
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-26 20:14:17 +0300

    Регистрация сервисов бизнес-логики в проекте хоста WebAPI

---
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/employees")] // http://localhost:5001/api/employees
+    public class EmployeesApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Services/WebStore.WebAPI/Program.cs
+++ Services/WebStore.WebAPI/Program.cs
@@ -1,3 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Services.Interfaces;
+using WebStore.Services;
+using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Services.InCookies;
+using WebStore.Services.InSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +17,38 @@ services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+
+services.AddScoped<IEmployeesData, SqlEmployeesData>();
+services.AddScoped<IProductData, SqlProductData>(); // !!! AddScoped !!!
+services.AddScoped<IOrderService, SqlOrderService>();
+
+services.AddIdentity<User, Role>()
+   .AddEntityFrameworkStores<WebStoreDB>()
+   .AddDefaultTokenProviders(); // Подключаем Identity
+
+services.Configure<IdentityOptions>(opt =>
+{
+#if DEBUG // условная компиляция
+    opt.Password.RequireDigit = false; // Пароль можно без цифр
+    opt.Password.RequireLowercase = false; // Требования к нижнему регистру
+    opt.Password.RequireUppercase = false; // Требования к верхнему регистру
+    opt.Password.RequireNonAlphanumeric = false; // Отключаем неалфавитные символы
+    opt.Password.RequiredLength = 3; // Минимальная длина - 3
+    opt.Password.RequiredUniqueChars = 3; // Как минимум три уникальных символа
+#endif
+
+    opt.User.RequireUniqueEmail = false; // Не нужны уникальные email
+    opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ1234567890";
+    //Разрешенные символы 
+    opt.Lockout.AllowedForNewUsers = false; // Отключаем автоматическую блокировку зарегистрированного пользователя
+    opt.Lockout.MaxFailedAccessAttempts = 10; // 10 попыток "угадать" пароль
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15); // Если 10 попыток неверны, то блокируем попытки на 15 минут
+});
+
+services.AddDbContext<WebStoreDB>(opt =>
+opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"))); // Подключение БД к сервисам
+
+services.AddTransient<IDbInitializer, DbInitializer>(); // Регистрируем сервис заполнения данных в бд
 
 var app = builder.Build();
 
