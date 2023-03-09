701bf6f8ec5858911d4ce64bd7453fd4b4c93ca7
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-09 13:48:11 +0300

    DTO-моделя для WebAPI Identity

---
+++ Common/WebStore.Domain (2)/DTO/Identity/ClaimDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+namespace WebStore.Domain.DTO.Identity
+{
+	public abstract class ClaimDTO : UserDTO
+	{
+		public IEnumerable<Claim> Claims { get; set; }
+	}
+
+	public class AddClaimDTO : ClaimDTO { }
+
+	public class RemoveClaimDTO : ClaimDTO { }
+
+	public class RemplaceClaimDTO : UserDTO
+	{
+		public Claim Claim { get; set; }
+		public Claim NewClaim { get; set; }
+	}
+
+
+}
---
+++ Common/WebStore.Domain (2)/DTO/Identity/UserDTO.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Domain.DTO.Identity
+{
+	public abstract class UserDTO
+	{
+		public User User { get; set; }
+	}
+
+	public class AddLoginDTO : UserDTO
+	{
+		public UserLoginInfo UserLoginInfo { get; set; }
+	}
+
+	public class PasswordHashDTO : UserDTO
+	{
+		public string Hash { get; set; }
+	}
+
+	public class SetLockoutDTO : UserDTO
+	{
+		public DateTimeOffset? LockoutEnd { get; set; }
+	}
+
+}
